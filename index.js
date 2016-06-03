ad2d67682f2e244e51b3fa897680ba8e471a8cda
Stan Hutcheon <stanhutcheon@bigfork.co.uk>
2016-06-03 13:23:53 +0100

    prevent navigation when contenteditable element is focused

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.addEventListener('keydown', function(e) {
 	var curr = document.activeElement;
-	e.keyCode === 8 && (!(curr instanceof HTMLInputElement) && !(curr instanceof HTMLTextAreaElement)) && history.back();
+	e.keyCode === 8 && ( !(curr instanceof HTMLInputElement) && !(curr instanceof HTMLTextAreaElement) && curr.getAttribute('contenteditable') === null ) && history.back();
 })
